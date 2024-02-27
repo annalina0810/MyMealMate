ac5873432bd50fca63fb0ac8121a16b94d1f651c
Callum Cooper Lee <2758297C@student.gla.ac.uk>
2024-02-27 17:29:18 +0000

    Added URL patterns based on naming in views.py

--- MyMealMate/urls.py
+++ MyMealMate/urls.py
@@ -5,4 +5,15 @@ app_name = 'MyMealMate'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('user_hub/', views.user_hub, name='user_hub'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit_profile/', views.edit_profile, name='edit_profile'),
+    path('my_meals/', views.my_meals, name='my_meals'),
+    path('my_meals/new_meal/', views.new_meal, name='new_meal'),
+    path('my_meals/<slug:meal_name_slug>/', views.meal, name='meal'),
+    path('my_meals/<slug:meal_name_slug>/edit_meal/', views.edit_meal, name='edit_meal'),
+    path('schedule/', views.schedule, name='schedule'),
+    path('shopping_list/', views.shopping_list, name='shopping_list'),
+    path('shopping_list/edit_shopping_list/', views.edit_shopping_list, name='edit_shopping_list'),
 ]
