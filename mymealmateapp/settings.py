d2d0bdeed3fbb9a9f619b660d0e889cfd96266e7
“Anna <2944538k@student.gla.ac.uk>
2024-03-19 17:39:09 +0000

    setup for python anywhere

--- mymealmateapp/settings.py
+++ mymealmateapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '09y5gkcleoe%yjj8&gze5#hfop=gcz=#xbxrh97$$r%!$$56bp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
