b8e56c255c85733faa1b7dc770f1aa093121ef5e
Katie Thomas <2756607t@student.gla.ac.uk>
2024-03-19 15:57:19 +0000

    tests

--- MyMealMate/tests.py
+++ MyMealMate/tests.py
@@ -1,18 +1,20 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from django.urls import reverse
+from MyMealMate.models import UserProfile
 
 # Create your tests here.
-class LoginTest(TestCase):
+class LoginTests(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-    
+        self.test_user = User.objects.create_user('testUser', 'test@test.com', 'testPassword')
+        UserProfile.objects.create(user=self.test_user)
+
     def test_login_success(self):
-        response = self.client.post("{% url 'MyMealMate: home}' %}", {'username': 'testuser', 'password': 'testpassword'})
+        response = self.client.post(reverse('MyMealMate:home'), {'username': 'testUser', 'password': 'testPassword'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, "{% url 'MyMealMate: user_hub}' %}")
+        self.assertEqual(response.url, reverse('MyMealMate:user_hub'))
 
     def test_login_fail(self):
-        response = self.client.post("{% url 'MyMealMate: home}' %}", {'username': 'wronguser', 'password': 'wrongpassword'})
+        response = self.client.post(reverse('MyMealMate:home'), {'username': 'wronguser', 'password': 'wrongpassword'})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.url, "{% url 'MyMealMate: home}' %}")
