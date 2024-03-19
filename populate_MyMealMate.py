0c4dee981b5e51a11b1dd93f2c3eb32eb35902b9
“Anna <2944538k@student.gla.ac.uk>
2024-03-19 09:51:52 +0000

    updated population script again

--- populate_MyMealMate.py
+++ populate_MyMealMate.py
@@ -5,6 +5,7 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mymealmateapp.settings')
 django.setup()
 
+from django.core.files import File
 from django.contrib.auth import get_user_model
 from MyMealMate.models import *
 
@@ -29,17 +30,27 @@ def populate():
     user_schedule.save()
 
     meals = [{"name": "Spaghetti Bolognese", "url": "https://www.recipetineats.com/spaghetti-bolognese/",
-              "instructions": "Cook with love", "image": None},
-             {"name": "Pizza", "url": None, "instructions": None, "image": None},
-             {"name": "Chili con Carne", "url": None, "instructions": None, "image": None},
-             {"name": "Porridge", "url": None, "instructions": None, "image": None},
+              "instructions": "Cook with love", "image": "spaghetti-bolognese.jpg"},
+             {"name": "Pizza", "url": None, "instructions": None, "image": "pizza.jpg"},
+             {"name": "Chili con Carne", "url": None, "instructions": None, "image": "chili-con-carne.jpg"},
+             {"name": "Porridge", "url": None, "instructions": None, "image": "porridge.jpg"},
              ]
 
-    ingredients = [{"name": "Spaghetti", "amount": "300", "unit": "g"},
+    spaghetti_ingredients = [{"name": "Spaghetti", "amount": "300", "unit": "g"},
                    {"name": "Minced Meat", "amount": "500", "unit": "g"},
                    {"name": "Tomato Sauce", "amount": "1", "unit": "kg"},
                    {"name": "Parmesan", "amount": "1", "unit": None},
+                   {"name": "Onions", "amount": "2", "unit": None},
+                   {"name": "Garclic cloves", "amount": "3", "unit": None},
+                   ]
+    chili_ingredients = [{"name": "Minced Meat", "amount": "500", "unit": "g"},
                    {"name": "Tomato Sauce", "amount": "300", "unit": "ml"},
+                   {"name": "Onions", "amount": "2", "unit": None},
+                   {"name": "Garclic cloves", "amount": "3", "unit": None},
+                   {"name": "Kidney Beans", "amount": "1", "unit": "can"},
+                   {"name": "Red pepper", "amount": "1", "unit": None},
+                   {"name": "Beef stock cube", "amount": "1", "unit": None},
+                   {"name": "Sweetcorn", "amount": "1", "unit": "can"},
                    ]
 
     def add_meal(name, url, image, instructions):
@@ -47,33 +58,36 @@ def populate():
         if url is not None:
             m.url = url
         if image is not None:
-            m.image = image
+            m.image.save(image, File(open(f'media/meal_images/{image}', 'rb')))
         if instructions is not None:
             m.instructions = instructions
         m.save()
         return m
 
     def add_ingredient(meal, name, amount, unit):
-        i = Ingredient.objects.get_or_create(meal=meal, name=name)[0]
+        i = Ingredient.objects.get_or_create(name=name)[0]
         if amount is not None:
             i.amount = amount
         if unit is not None:
             i.unit = unit
         i.save()
+        meal.ingredients.add(i)
         return i
 
     # add Meals
     for m in meals:
-        add_meal(m["name"], m["image"], m["url"], m["instructions"])
+        add_meal(m["name"], m["url"], m["image"], m["instructions"])
 
     # add Ingredients
-    for i in ingredients[:-1]:
+    for i in spaghetti_ingredients:
         add_ingredient(Meal.objects.get(name="Spaghetti Bolognese"), i["name"], i["amount"], i["unit"])
-    add_ingredient(Meal.objects.get(name="Pizza"), ingredients[-1]["name"], ingredients[-1]["amount"], ingredients[-1]["unit"])
+    for i in chili_ingredients:
+        add_ingredient(Meal.objects.get(name="Chili con Carne"), i["name"], i["amount"], i["unit"])
 
     # add ingredients to shopping list
-    for i in Ingredient.objects.all():
-        user_shopping_list.add_ingredient(i)
+    if not ShoppingListItem.objects.all().exists():
+        for i in Ingredient.objects.all():
+            user_shopping_list.add_ingredient(i)
 
     # schedule all Meals for today
     new_day = Day(schedule=user_schedule, date=datetime.date.today())
